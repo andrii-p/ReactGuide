dea53ad7eab55f27e3502f058d61a30447a53a6d
Andrii Pelekh <andrii.pelekh@nasdaq.com>
2017-06-13 16:49:31 -0600

    Adding ROME

--- src/main/java/com/example/ReactExample/ReactExampleApplication.java
+++ src/main/java/com/example/ReactExample/ReactExampleApplication.java
@@ -1,12 +1,38 @@
 package com.example.ReactExample;
 
+import java.net.URL;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
 
 @SpringBootApplication
 public class ReactExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactExampleApplication.class, args);
+		
+		if (args.length < 1){
+			System.out.println("NO ARGUMENT");
+		} else {
+			try {				
+				URL url = new URL(args[0]);
+				
+				System.out.println("READING URL - " + url);
+				
+				SyndFeedInput input = new SyndFeedInput();
+				SyndFeed feed = input.build(new XmlReader(url));
+				
+				System.out.println(feed);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}				
+		}
+		
+		
+		
 	}
 }
