b732ac45ddbc67e049467d69ec0e1ce0dc44521b
Andrii Pelekh <andrii.pelekh@nasdaq.com>
2017-12-08 15:54:35 -0700

    Equals method template

---
+++ misc/Animal.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Animal {
+
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        com.company.Animal other = (com.company.Animal) o;
+        if (isOneNullOtherNotNull(this.getName(), other.getName()) || isFirstNotEqualsSecond(this.getName(), other.getName())) return false;
+
+        return true;
+    }
+
+    private boolean isOneNullOtherNotNull(Object x, Object y){
+        return (x == null && y != null) || (x != null && y == null);
+    }
+
+    private boolean isFirstNotEqualsSecond(Object first, Object second){
+        return first != null && !first.equals(second);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
